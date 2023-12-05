a9b155bb85a193813dc8fb3f7d5612bfb664a032
Girish-120 <98391965+Girish-120@users.noreply.github.com>
2023-12-05 12:56:58 +0530

    old changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   // siteUrl:"https://laxmi-prabhu.onrender.com/api/",
   // imageurl:"https://laxmi-prabhu.onrender.com/",
-  siteUrl:"http://192.168.1.31:3000/api/",
-  imageurl:"http://192.168.1.31:3000/"
+  siteUrl:"http://192.168.1.22:3000/api/",
+  imageurl:"http://192.168.1.22:3000/"
 };
 
 /*
