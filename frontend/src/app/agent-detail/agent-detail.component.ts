155d5344174279745d52c60b1a82a87f94f9e088
Girish-120 <98391965+Girish-120@users.noreply.github.com>
2023-12-29 12:45:39 +0530

    code pushed and changes done

--- frontend/src/app/agent-detail/agent-detail.component.ts
+++ frontend/src/app/agent-detail/agent-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { AppserviceService } from '../appservice.service';
 import Swal from 'sweetalert2';
 import { environment } from 'src/environments/environment';
+import { Validators,FormBuilder } from '@angular/forms';
+declare var $:any;
 
 @Component({
   selector: 'app-agent-detail',
@@ -14,8 +16,9 @@ export class AgentDetailComponent implements OnInit {
   agentId: any;
   agentDetail: any;
   imageUrl = environment.imageurl;
+  submitForm:boolean = false;
 
-  constructor(private route:ActivatedRoute,private service:AppserviceService) { 
+  constructor(private route:ActivatedRoute,private service:AppserviceService,private fb:FormBuilder) { 
     this.route.paramMap.subscribe((params: any) => {
       this.agentId = params.get('id');
     })
@@ -41,6 +44,58 @@ export class AgentDetailComponent implements OnInit {
         icon: 'error',
         });
     })
+  }
+
+  agentQuery = this.fb.group({
+    name:["",[Validators.required]],
+    email:["",[Validators.required]],
+    mobile:["",[Validators.required]],
+    agentID:["",[Validators.required]],
+    message:[""]
+  })
+
+  submitQuery(){
+    this.agentQuery.patchValue({"agentID":this.agentId});
+    if(this.agentQuery.valid){
+      this.service.post("agent-query",this.agentQuery.value).subscribe((res:any)=>{
+        if (res.success == true) {
+          Swal.fire({
+            title: 'Thankyou',
+            text: "Thankyou for submitting your detail with us for Agent. our Agent will contact you shortly.",
+            imageUrl: '/assets/img/thanks.png',
+            imageHeight:"160px",
+            showCancelButton: false, 
+            showConfirmButton: false
+          });
+          setTimeout(() => {
+            Swal.close();
+          }, 3000);
+          this.isLoading = false;
+          $("#login").modal("hide");
+          this.agentQuery.reset();
+
+        } else {
+          setTimeout(() => {
+            this.isLoading = false;
+          }, 1000);
+          Swal.fire({
+            title: 'Error',
+            text: res.message,
+            icon: 'error',
+          });
+        }
+      }, error => {
+        this.isLoading = false;
+        Swal.fire({
+          title: 'Error',
+          text: error.error.message,
+          icon: 'error',
+        });
+      })
+    } else {
+      this.submitForm = true;
+      this.isLoading = false;
+    }
   }
 
 }
